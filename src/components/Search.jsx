40d083d82f9630e54cd2a5c160a907b686ccde6b
Olosko23 <oloogeorge633@gmail.com>
2023-02-20 20:26:26 +0300

    '5'

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -7,30 +7,31 @@ const Search = () => {
   return (
     <div className="searchDiv grid gap-10 bg-greyIsh rounded-[10px] p-[3rem]">
       
-      <form action=''>
-        <div className="firstDiv flex justify-between items-center rounded-[8px] gap-[10px] bg-white p-5 shadow-lg shadow-greyIsh-700">
-          <div className="flex gap-2 items-center">
-            <AiOutlineSearch className="text-[25px] icon"/>
+      <form className="" action=''>
+        <div className="firstDiv flex flex-col md:flex-row justify-between items-center rounded-[8px] gap-[10px] bg-white p-5 shadow-lg shadow-greyIsh-700">
+          <div className="flex w-3/4 gap-5 items-center">
+            <AiOutlineSearch className="text-[30px] icon"/>
             <input type="text" className="bg-transparent text-blue-500 focus:outline-none w-[100%]" placeholder='Search Job Here...' />
-            <AiOutlineCloseCircle className="text-[30px] text-[#a5a6a6] hover:text-textColor icon" />
+            <button type="reset"><AiOutlineCloseCircle className="text-[30px] text-[#a5a6a6] hover:text-textColor icon" /></button>
           </div>
-          <div className="flex gap-2 items-center">
-            <BsHouseDoor className="text-[25px] icon"/>
+          <div className="flex w-3/4 gap-5 items-center">
+            <BsHouseDoor className="text-[30px] icon"/>
             <input type="text" className="bg-transparent text-blue-500 focus:outline-none w-[100%]" placeholder='Search by Company..' />
-            <AiOutlineCloseCircle className="text-[30px] text-[#a5a6a6] hover:text-textColor icon" />
+            <button type="reset"><AiOutlineCloseCircle className="text-[30px] text-[#a5a6a6] hover:text-textColor icon" /></button>
           </div>
-          <div className="flex gap-2 items-center">
-            <CiLocationOn className="text-[25px] icon"/>
+          <div className="flex w-3/4 gap-5 items-center">
+            <CiLocationOn className="text-[30px] icon"/>
             <input type="text" className="bg-transparent text-blue-500 focus:outline-none w-[100%]" placeholder='Search by Location' />
-            <AiOutlineCloseCircle className="text-[30px] text-[#a5a6a6] hover:text-textColor icon" />
+            <button type="reset"><AiOutlineCloseCircle className="text-[30px] text-[#a5a6a6] hover:text-textColor icon" /></button>
           </div>
           <button className="bg-blueColor h-full p-5 px-10 rounded-[10px] text-white cursor-pointer hover:bg-blue-300">Search</button>
         </div>
       </form>
-      <div className="secDiv flex items-center gap-10 justify-center">
+      <form className="secDiv flex flex-col md:flex-row items-center gap-10 justify-center">
         <div className="singleSearch flex items-center gap-2">
             <label htmlFor="relevance" className="text-[#808080] font-semibold">Sort by:</label>
             <select name="" id="relevance" className="bg-white rounded-[3px] px-4 py-1">
+              <option value="">--</option>
               <option value="">Relevance</option>
               <option value="">Inclusive</option>
               <option value="">Starts With</option>
@@ -40,6 +41,7 @@ const Search = () => {
         <div className="singleSearch flex items-center gap-2">
             <label htmlFor="type" className="text-[#808080] font-semibold">Type:</label>
             <select name="" id="type" className="bg-white rounded-[3px] px-4 py-1">
+              <option value="">--</option>
               <option value="">Full-Time</option>
               <option value="">Remote</option>
               <option value="">Contract</option>
@@ -49,14 +51,15 @@ const Search = () => {
         <div className="singleSearch flex items-center gap-2">
             <label htmlFor="level" className="text-[#808080] font-semibold">Level:</label>
             <select name="" id="level" className="bg-white rounded-[3px] px-4 py-1">
+              <option value="">--</option>
               <option value="">Senior</option>
               <option value="">Beginner</option>
               <option value="">Intermediate</option>
               <option value="">Advocate</option>
             </select>
         </div>
-        <span className="text-[#a1a1a1] cursor-pointer">Clear All</span>
-      </div>
+        <button type="reset" className="text-[#a1a1a1] cursor-pointer">Clear All</button>
+      </form>
     </div>
   )
 }
