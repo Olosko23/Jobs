2d842feb767306194270ed9f76e399959fa22ab4
Olosko23 <oloogeorge633@gmail.com>
2023-02-23 10:02:12 +0300

    '13'

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import {FaBars, FaTimes} from 'react-icons/fa';
+import {Linknk} from 'react-scroll';
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -12,27 +13,27 @@ const Navbar = () => {
         </h1>
       </div>
       <div className="menu hidden md:flex gap-8">
-        <li className="menuList text-[#6f6f6f] hover:text-blueColor">Jobs</li>
-        <li className="menuList text-[#6f6f6f] hover:text-blueColor">Companies</li>
-        <li className="menuList text-[#6f6f6f] hover:text-blueColor">About</li>
-        <li className="menuList text-[#6f6f6f] hover:text-blueColor">Contact</li>
-        <li className="menuList text-[#6f6f6f] hover:text-blueColor">Blog</li>
-        <li className="menuList text-[#6f6f6f] hover:text-blueColor">Login</li>
-        <li className="menuList text-[#6f6f6f] hover:text-blueColor">Register</li>
+        <Link  to="jobs" smooth className="menuList text-[#6f6f6f] hover:text-blueColor">Jobs</Link>
+        <Link to="jobs" smooth  className="menuList text-[#6f6f6f] hover:text-blueColor">Companies</Link>
+        <Link to="jobs" smooth  className="menuList text-[#6f6f6f] hover:text-blueColor">About</Link>
+        <Link  to="jobs" smooth className="menuList text-[#6f6f6f] hover:text-blueColor">Contact</Link>
+        <Link to="jobs" smooth  className="menuList text-[#6f6f6f] hover:text-blueColor">Blog</Link>
+        <Link to="jobs" smooth  className="menuList text-[#6f6f6f] hover:text-blueColor">Login</Link>
+        <Link to="jobs" smooth  className="menuList text-[#6f6f6f] hover:text-blueColor">Register</Link>
       </div>
-      <div onClick={() => setNav(!nav)} className="pr-2 z-10 cursor-pointer flex md:hidden text-blueColor">
+      <div onCLinkck={() => setNav(!nav)} className="pr-2 z-10 cursor-pointer flex md:hidden text-blueColor">
         {nav ? <FaTimes /> : <FaBars />}
       </div>
       {nav && (
         <div className="flex w-full h-screen flex-col justify-center bg-blue-200 items-center top-0 right-0 gap-2">
-        <li className="menuList text-[#6f6f6f] px-4 cursor-pointer capitalize py-6 text-xl bg-white opacity-50 hover:text-blueColor">Jobs</li>
-        <li className="menuList text-[#6f6f6f] px-4 cursor-pointer capitalize py-6 text-xl bg-white opacity-50 hover:text-blueColor">Companies</li>
-        <li className="menuList text-[#6f6f6f] px-4 cursor-pointer capitalize py-6 text-xl bg-white opacity-50 hover:text-blueColor">About</li>
-        <li className="menuList text-[#6f6f6f] px-4 cursor-pointer capitalize py-6 text-xl bg-white opacity-50 hover:text-blueColor">Contact</li>
-        <li className="menuList text-[#6f6f6f] px-4 cursor-pointer capitalize py-6 text-xl bg-white opacity-50 hover:text-blueColor">Blog</li>
-        <li className="menuList text-[#6f6f6f] px-4 cursor-pointer capitalize py-6 text-xl bg-white opacity-50 hover:text-blueColor">Blog</li>
-        <li className="menuList text-[#6f6f6f] px-4 cursor-pointer capitalize py-6 text-xl bg-white opacity-50 hover:text-blueColor">Login</li>
-        <li className="menuList text-[#6f6f6f] px-4 cursor-pointer capitalize py-6 text-xl bg-white opacity-50 hover:text-blueColor">Register</li>
+        <Link to="jobs" smooth className="menuList text-[#6f6f6f] px-4 cursor-pointer capitalize py-6 text-xl bg-white opacity-50 hover:text-blueColor">Jobs</Link>
+        <Link to="jobs" smooth className="menuList text-[#6f6f6f] px-4 cursor-pointer capitalize py-6 text-xl bg-white opacity-50 hover:text-blueColor">Companies</Link>
+        <Link to="jobs" smooth className="menuList text-[#6f6f6f] px-4 cursor-pointer capitalize py-6 text-xl bg-white opacity-50 hover:text-blueColor">About</Link>
+        <Link to="jobs" smooth className="menuList text-[#6f6f6f] px-4 cursor-pointer capitalize py-6 text-xl bg-white opacity-50 hover:text-blueColor">Contact</Link>
+        <Link to="jobs" smooth className="menuList text-[#6f6f6f] px-4 cursor-pointer capitalize py-6 text-xl bg-white opacity-50 hover:text-blueColor">Blog</Link>
+        <Link to="jobs" smooth className="menuList text-[#6f6f6f] px-4 cursor-pointer capitalize py-6 text-xl bg-white opacity-50 hover:text-blueColor">Blog</Link>
+        <Link to="jobs" smooth className="menuList text-[#6f6f6f] px-4 cursor-pointer capitalize py-6 text-xl bg-white opacity-50 hover:text-blueColor">Login</Link>
+        <Link to="jobs" smooth className="menuList text-[#6f6f6f] px-4 cursor-pointer capitalize py-6 text-xl bg-white opacity-50 hover:text-blueColor">Register</Link>
       </div>
       )}
     </div>
--- src/components/Value.jsx
+++ src/components/Value.jsx
@@ -5,7 +5,7 @@ import {MdOutlineDetails} from 'react-icons/md';
 
 const Value = () => {
   return (
-    <div className="mb-[4rem] mt-[6rem]">
+    <div name="value" className="mb-[4rem] mt-[6rem]">
       <h1 className="text-textColor text-[25px] py-[2rem] pb-[3rem] font-bold w-[400px] block ">
         The value that holds us true to our account
       </h1>
