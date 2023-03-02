2e5d3a8e01393e6ffe1d316388be1f3fe7cf79ec
Olosko23 <oloogeorge633@gmail.com>
2023-03-02 13:24:52 +0300

    'day2'

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,22 @@ import React from 'react'
 import Navbar from './components/Navbar';
 import Search from './components/Search';
 import Jobs from './components/Jobs';
+import Job from './components/Job';
 import Value from './components/Value';
 import Footer from './components/Footer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="w-[85%] m-auto bg-white">
       <Navbar />
       <Search />
-      <Jobs />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Jobs />} />
+          <Route path='/jobs/:id' element={<Job />} />
+        </Routes>
+      </BrowserRouter>
       <Value />
       <Footer />
     </div>
